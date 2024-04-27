14b7380f616e82402a8a55e5e2dffe78d4f5e399
szy.szu <szy.szumski@gmail.com>
2024-04-27 13:52:42 +0200

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,27 @@ import { Input } from "./components/Input/Input";
 import { Link } from "./components/Link/Link";
 import { List } from "./components/List/List";
 import { AppContext, AppContextProvider } from "./context/AppContext";
+import { ThemeContext } from "./context/ThemeContext";
 
 function App() {
   const { count, handleCount } = useContext(AppContext);
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundColor: isDarkMode ? "black" : "white",
+        color: isDarkMode ? "white" : "black",
+      }}
+    >
       <Banner text="nasz text dla bannera" />
       <Button>nasz tekst1</Button>
       <button onClick={() => handleCount(5)}>Dodaj count globalny</button>
       <p>{count}</p>
+      <button onClick={toggleTheme}>
+        Włącz tryb: {isDarkMode ? "Jasny" : "ciemny"}
+      </button>
       <Link href="www.google.com">1231</Link>
       <Input />
       <Form />
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppContextProvider } from "./context/AppContext";
+import { ThemContextProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
+      <ThemContextProvider>
+        <App />
+      </ThemContextProvider>
     </AppContextProvider>
   </React.StrictMode>
 );
