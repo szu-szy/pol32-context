e88b0d4bf79ee1a3a7b762e366e6206ed5d6423a
szy.szu <szy.szumski@gmail.com>
2024-04-27 12:11:25 +0200

    init context

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import "./App.css";
 import { Banner } from "./components/Banner/Banner";
 import { Button } from "./components/Button/Button";
@@ -5,13 +6,17 @@ import { Form } from "./components/Form/Form";
 import { Input } from "./components/Input/Input";
 import { Link } from "./components/Link/Link";
 import { List } from "./components/List/List";
+import { AppContext, AppContextProvider } from "./context/AppContext";
 
 function App() {
+  const { count, handleCount } = useContext(AppContext);
   return (
     <div className="App">
       <Banner text="nasz text dla bannera" />
       <Button>nasz tekst1</Button>
-      <Link href="www.google.com">Go to google</Link>
+      <button onClick={() => handleCount(5)}>Dodaj count globalny</button>
+      <p>{count}</p>
+      <Link href="www.google.com">1231</Link>
       <Input />
       <Form />
       <List />
